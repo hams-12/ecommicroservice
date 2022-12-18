550ca23448c9f03f44528da3d8f45243249c0771
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2022-12-18 11:21:50 +0100

    Premier commit avec ajout des entitiés

---
+++ inventory-service/src/main/java/org/sid/inventoryservice/entites/Category.java
@@ -0,0 +1,19 @@
+package org.sid.inventoryservice.entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
