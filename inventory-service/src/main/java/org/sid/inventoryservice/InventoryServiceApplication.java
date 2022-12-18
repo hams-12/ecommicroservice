c599276f786d4bb3d1b3f7537c751ddff121e568
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2022-12-18 11:53:38 +0100

    Ajout des repositories,d'un code de test et modif du fichier application.properties

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,47 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.Category;
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repositories.CategoryRepository;
+import org.sid.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(CategoryRepository categoryRepository,ProductRepository productRepository){
+        return args -> {
+            Random random = new Random();
+            List.of("Computer","Printer","Smartphone").forEach(cat->{
+                Category category = Category.builder().name(cat).build();
+                categoryRepository.save(category);
+            });
+            categoryRepository.findAll().forEach(cate -> {
+                for (int i = 0; i < 10; i++) {
+                    Product product = Product.builder()
+                            .id(UUID.randomUUID().toString())
+                            .name("Computer "+i)
+                            .price(100+Math.random()*5000)
+                            .quantity(random.nextInt(100))
+                            .category(cate)
+                            .build();
+                    productRepository.save(product);
+                }
+            });
+
+
+        };
     }
 
 }
