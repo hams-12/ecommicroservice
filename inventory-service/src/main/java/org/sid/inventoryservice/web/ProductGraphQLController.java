d1d4e98baec549f144c4324686eb10834c96995b
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2022-12-19 23:45:18 +0100

    Ajout de la mutation saveProduct et modification du schema graphQL version1

--- inventory-service/src/main/java/org/sid/inventoryservice/web/ProductGraphQLController.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/web/ProductGraphQLController.java
@@ -4,10 +4,12 @@ import org.sid.inventoryservice.entities.Product;
 import org.sid.inventoryservice.repositories.ProductRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class ProductGraphQLController {
@@ -24,5 +26,11 @@ public class ProductGraphQLController {
     @QueryMapping
     public Product productById(@Argument String id){
         return productRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Product %s not found",id)));
+    }
+
+    @MutationMapping
+    public Product saveProduct(@Argument Product product){
+        product.setId(UUID.randomUUID().toString());
+        return productRepository.save(product);
     }
 }
